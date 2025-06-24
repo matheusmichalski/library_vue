f542e3b81b2b833f6c49221d497baf50a2dbb1a2
nicolefemello <nicfmello@gmail.com>
2025-06-24 14:33:31 -0300

    FEAT: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,50 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Library',
+        short_name: 'Library',
+        description: 'Library: IFbooks',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        id: 'com.fake-store.app',
+        orientation: 'any',
+        background_color: '#ffffff',
+        start_url: '.',
+        launch_handler: {
+          client_mode: ['navigate-existing', 'auto'],
+        },
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
